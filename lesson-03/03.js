20bd46a3a03cdd871a2f26764b00e6f620c8f39b
Rais-Tahirovich <ipgaynullin@gmail.com>
2025-11-24 18:46:39 +0400

    DZ3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,9 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(a, b, c) {
+    return Math.max(a, b, c);
+}
+
+console.log(findLargest(10, 20, 15));  // 20
+console.log(findLargest(5, 5, 3));     // 5
+console.log(findLargest(-1, -5, 0));   // 0
